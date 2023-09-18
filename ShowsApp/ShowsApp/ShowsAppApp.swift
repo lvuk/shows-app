b7064773bab68fdba7058d8575984c6a947c963a
lvuk <luka.vuk456@gmail.com>
2023-09-18 12:53:29 +0200

    project created

---
+++ ShowsApp/ShowsApp/ShowsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowsAppApp.swift
+//  ShowsApp
+//
+//  Created by Luka Vuk on 18.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct ShowsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
