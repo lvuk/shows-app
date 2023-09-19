80d8432f303d2940d04ab0ab5577c6d76152a9c3
lvuk <luka.vuk456@gmail.com>
2023-09-19 11:44:40 +0200

    show model

---
+++ ShowsApp/ShowsApp/Models/Show.swift
@@ -0,0 +1,24 @@
+//
+//  Show.swift
+//  ShowsApp
+//
+//  Created by Luka Vuk on 19.09.2023..
+//
+
+import Foundation
+import SwiftUI
+
+struct Show: Codable, Identifiable {
+    struct Schedule: Codable {
+        let time: String
+        let days: [String]
+    }
+    
+    let id: Int
+    let url: String
+    let name: String
+    let type: String
+    let language: String
+    let genres: [String]
+    let schedule: Schedule
+}
